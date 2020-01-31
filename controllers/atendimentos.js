79f202adc7cb43467b6bf3de4bbec70f3e98289d
Luiz Henrique Leme <lheme@outlook.com.br>
2020-01-31 20:34:15 -0300

    Criada a rota do POST. Testar com POSTMAN

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,4 +1,7 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) =>
-    res.send('Servidor rodando, tudo okay! Você está na rota de atendimentos e está realizando um GET'))
+        res.send('Servidor rodando, tudo okay! Você está na rota de atendimentos e está realizando um GET'))
+
+    app.post('/atendimentos', (req, res) =>
+        res.send('Você está na rota de atendimentos e está realizando um POST'))
 }
