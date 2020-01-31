31f995dd2c3f6f4ecd1a035895023daab7c4a778
Luiz Henrique Leme <lheme@outlook.com.br>
2020-01-31 20:48:41 -0300

    Instalado o Body-Parser e implementado o tratamento para ler o conteudo do corpo da requisicao

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,6 +2,9 @@ module.exports = app => {
     app.get('/atendimentos', (req, res) =>
         res.send('Servidor rodando, tudo okay! Você está na rota de atendimentos e está realizando um GET'))
 
-    app.post('/atendimentos', (req, res) =>
-        res.send('Você está na rota de atendimentos e está realizando um POST'))
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Você está na rota de atendimentos e está realizando um POST')
+    })
+
 }
